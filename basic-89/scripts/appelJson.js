3c8d51f9121086e105d7ab9acc026ee35eaf4008
doud94220 <edouard.anthony@gmail.com>
2017-06-16 13:53:03 +0200

    avecAppelJson_EXo3et4
    avecAppelJson_EXo3et4

--- basic-89/scripts/appelJson.js
+++ basic-89/scripts/appelJson.js
@@ -1,11 +1,16 @@
+////////////////////////////////////////////// EXO 2, 3, 4 //////////////////////////////////////////////
+
+
 $(function() //déclenchée lors de chargement total du DOM
 {
+	////////// EXO 2 : Appel AJAX //////////
+
 	var appelJsonElmt = $("#appelJson");
 
 	var request = $.ajax(
 	{
 	  url: "https://jsonplaceholder.typicode.com/users",
-	  method: "GET";
+	  method: "GET",
 	  dataype : "json" //Optionnel
 	});
 	 
@@ -19,10 +24,41 @@ $(function() //déclenchée lors de chargement total du DOM
 		// }
 
 		///// AFFICHAGE des 'name' du json AVEC UN FOREACH
+		var content = ""; //Va stocker les infos json clickables
+
 		jsonData.forEach(function(unElmtDeJsonData)
 		{
  	 		var nameAjaxElmt = unElmtDeJsonData.name;
-			$('<p>').html(nameAjaxElmt).css('color', 'blue').appendTo(appelJsonElmt);
+			content += '<li id="User-' + unElmtDeJsonData.id + '"><a href="#">' + unElmtDeJsonData.name + '</a></li>';
+		});
+
+		$("#appelJson ul").html(content); //Creation des li dans le ul de l'id appelJson
+
+
+		////////// EXO 3 : Clic sur les liens json //////////
+
+		$("#appelJson ul > li").click(function(e)
+		{
+			e.preventDefault();
+
+			/** Recuperation de l'id **/
+			var idUser = $(this).attr("id"); //on recupere l'attribut id creer tout à l'heure dans le foreach
+			console.log(idUser.split("-")); // User-3 = array("User",3); // Couper par rapport au caratere '-'
+			idUser = idUser.split("-");
+
+			/** Requet Ajax ciblé sur un seul element **/
+			var ficheUserCible = $.ajax(
+			{
+				url: "https://jsonplaceholder.typicode.com/users",
+				method: "GET",
+				data: { id: idUser[1] },
+				dataType: "json" // optionel
+			});
+
+			ficheUserCible.done(function(dataUserCible)
+			{
+				console.info(dataUserCible[0].username+" "+dataUserCible[0].email);
+			});
 		});
 	});
 	 
@@ -31,5 +67,72 @@ $(function() //déclenchée lors de chargement total du DOM
 	  alert( "Request failed: " + textStatus );
 	});
 
-	console.log(request);
-});
+
+
+	////////// EXO 4 : Les 4 parties 'Lorem ipsum....' a remplacer par le body d'un new json //////////
+
+	var request2 = $.ajax(
+	{
+	  url: "https://jsonplaceholder.typicode.com/posts",
+	  method: "GET",
+	  dataype : "json" //Optionnel
+	});
+
+	request2.done(function(jsonData2)
+	{
+		// Remplacer les chaines 'Lorem ipsum...'
+		for(i=0;i<4;i++) //On boucle sur les 4 premiers elements du json retourné par request
+		{
+			var bodyAjaxElmt = jsonData2[i].body; //Recup du body
+			if(bodyAjaxElmt.length > 100) //Si y'a plus de 100 caractères dans le body
+			{
+				// bodyAjaxElmtTronque = bodyAjaxElmt.substring(0,100); // Je prends que les 100 premiers caractères ///Note : Attention substring() est une fonction javascript
+				bodyAjaxElmtTronque = bodyAjaxElmt.slice(0,100); //Je remplace le substring par slice
+				$('#services > article').eq(i).children('p').eq(0).text(bodyAjaxElmtTronque); //j'affiche que les 100 premiers caractères du "body json" dans le <p>
+				$('#services > article').eq(i).children('p').eq(0).data('bodyAjaxElmt', bodyAjaxElmt); //je stocke la chaine complete du "body json" dans le même <p> avec data()
+				$('#services > article').eq(i).children('p').eq(0).data('bodyAjaxElmtTronque', bodyAjaxElmtTronque); //je stocke la chaine tronquée du "body json" dans le même <p> avec data()
+			}
+			else
+			{
+				$('#services > article').eq(i).children('p').eq(0).text(bodyAjaxElmt); //j'affiche le "body json" pas tronqué
+				// Note : pas val() mais text()......je perds 15 min là-dessus
+			}
+
+			// Remplacer ReadMore par  ... dans les 4 zones
+			$('#services > article').eq(i).children('p').eq(1).children('a').text("...");
+		}
+
+		
+		// Gérer le CLICK sur les liens ... u ReadLess
+		$("#services > article > p > a").click(function(e)
+		{
+			e.preventDefault(); //Comme la cible du lien  = #, ca va juste empêcher de remonter en haut de page après le click
+
+			if ($(this).text() == "...")
+			{
+				//Afficher le "body json" pas tronqué dans le <p>
+				$(this).parent().prev().text($(this).parent().prev().data('bodyAjaxElmt')); //je recup le "body json" pas tronqué grâce à data()
+
+				//Remplacer le ... par un readLess
+				$(this).text("ReadLess");
+			}
+			else
+			{
+				//Afficher le text tronqué
+				$(this).parent().prev().text($(this).parent().prev().data('bodyAjaxElmtTronque')); //je recup le "body json" tronqué grâce à data()
+
+				//Remplacer le readLess par un "..."
+				$(this).text("...");
+			}
+		});
+
+		/// NOTE : LE PROF A GERER LE CLICK DANS LA BOUCLE FOR ///
+
+	});
+	 
+	request2.fail(function(jqXHR, textStatus)
+	{
+	  	alert( "Request 2 has failed: " + textStatus );
+	});
+
+})
--- basic-89/scripts/carroussel.js
+++ basic-89/scripts/carroussel.js
@@ -1,29 +1,11 @@
+//////////////// EXO 1 ////////////////
+
 $(function() //déclenchée lors de chargement total du DOM
 {
-	// MON CODE => A ESSAYER DE FAIRE MARCHE JEUDI SOIR
-	// let tabUrlImage = ['images/sport_1.jpg', 'images/sport_2.jpg', 'images/sport_3.jpg'];
-	// console.log(tabUrlImage);
-
-	// let carroussel = $("#slider");
-
-	// for(i=0;i<3;i++)
-	// {
-	// 	$(carroussel).animate(
-	// 	{
-	// 		carroussel.child().child().attr('src','images/sport_1.jpg');
-	// 	},1000);
-
-	// 	if(i==2)
-	// 	{
-	// 		i=0;
-	// 	}
-	// }
-
-
 	// CORRECTION PROF
 	let images = 
 		[
-			"images/sport_1.jpg",
+			"http://www.gynecolille.fr/upl/website/gynecologie-et-sport/7_srcset-large.jpg",
 			"http://www.be.fa.com/content/dam/fa/nl/nl/Q12016Innovaties/17_fa_master_stages_960x360_Douche-ActiveSport-NL.jpg",
 			"http://www.leporge-ecoledesurf.com/wp-content/uploads/2016/04/DSC02760.jpg"
 		]
